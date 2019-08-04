ec66e457c53f146e2c3124290401c78170b4ef83
zhangucan <zhangxiangguo@xiaoantech.com>
2019-08-05 00:04:13 +0800

    FIX: 加入mapbox，准备数据渲染

--- src/module.tsx
+++ src/module.tsx
@@ -1,9 +1,29 @@
 import React, { PureComponent } from 'react';
 import { PanelProps, PanelPlugin } from '@grafana/ui';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+const Map = ReactMapboxGl({
+  accessToken: 'pk.eyJ1Ijoiemhhbmd1Y2FuIiwiYSI6ImNqZ2t4d2hybTFoczEzM3BxZHNiZmx5ODEifQ.cRxbqbN3MrW454UdMfoc6w',
+});
 
 export class MyPanel extends PureComponent<PanelProps> {
+
+  componentDidMount() {
+    
+  }
   render() {
-    return <div>Hello from my panel</div>;
+    return (
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: '100vh',
+          width: '100vw',
+        }}
+      >
+        <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+          <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+        </Layer>
+      </Map>
+    );
   }
 }
 
